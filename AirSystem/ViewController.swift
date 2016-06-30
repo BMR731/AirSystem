51246c0b960aa77afcfa0a9390ea6fb9c7c1f3bf
BMR731 <1547963731@qq.com>
2016-06-30 15:51:36 +0800

    Initial Commit

---
+++ AirSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirSystem
+//
+//  Created by 胡胡赛军 on 6/30/16.
+//  Copyright © 2016 胡胡赛军. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
